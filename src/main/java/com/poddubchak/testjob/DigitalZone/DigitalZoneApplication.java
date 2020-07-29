23602e9fc5e62327cb0985d5bb065a630120f4ae
VladimirPoddubchak <45694858+VladimirPoddubchak@users.noreply.github.com>
2020-07-29 12:00:17 +1000

    Initial commit

---
+++ src/main/java/com/poddubchak/testjob/DigitalZone/DigitalZoneApplication.java
@@ -0,0 +1,13 @@
+package com.poddubchak.testjob.DigitalZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poddubchak/testjob/DigitalZone/DigitalZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poddubchak.testjob.DigitalZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
